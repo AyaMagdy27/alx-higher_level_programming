1cdb1d4c7277e3f9711093737c7d00191830f942
AyaMagdy27 <ayamado1996@gmail.com>
2024-01-22 18:21:07 -0800

    fix 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,9 @@
 def divisible_by_2(my_list=[]):
     if my_list is None:
         return None
+    listdiv = []
     for i in my_list:
         if (i % 2) == 0:
-            return True
+            listdiv.append(True)
         else:
-            return False
-
+            listdiv.append(False)
