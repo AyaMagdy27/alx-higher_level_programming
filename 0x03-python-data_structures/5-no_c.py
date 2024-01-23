8d9ff8db859d89b19e53a79ec664ed74c8dd3c05
AyaMagdy27 <ayamado1996@gmail.com>
2024-01-22 16:26:14 -0800

    fix 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    rep = ""
-    for l in range(len(my_string)):
-        if (my_string[l] != 'c' and my_string[l] != 'C'):
-            rep += my_string[l]
-    return rep
+    ret = ""
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            ret += my_string[i]
+    return ret
