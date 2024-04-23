ed6b448d1c96bfeb3275b619072eb92b0991c4c7
AyaMagdy27 <ayamado1996@gmail.com>
2024-04-22 20:53:55 -0700

    fix t 10, 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ const argumentsList = process.argv.slice(2);
 const numbers = argumentsList.map(arg => parseInt(arg));
 
 if (number.length < 2) {
-	console.log(0);
+  console.log(0);
 } else {
-	const sortedNumbers = numbers.sort((a, b) => b -a);
-	console.log(sortedNumbers[1]);
+  const sortedNumbers = numbers.sort((a, b) => b -a);
+  console.log(sortedNumbers[1]);
 }
