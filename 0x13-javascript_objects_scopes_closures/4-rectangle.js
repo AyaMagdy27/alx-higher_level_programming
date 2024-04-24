ef123813321d7f3946956e668ae399c827537bfc
AyaMagdy27 <ayamado1996@gmail.com>
2024-04-24 12:17:45 -0700

    task and adv

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,7 +10,7 @@ module.exports = class Rectangle {
 
   rotate () {
     [this.width, this.height] = [this.height, this.width];
-		    }
+  }
 
    double () {
      [this.width, this.height] = [this.width * 2, this.height * 2];
